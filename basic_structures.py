e7e0ad745fea19cc54bd3e7ada37243b9c064dba
zach <zachary@stoneumbrella.com>
2023-04-27 20:22:14 -0400

    basic_structures.py function edit

--- basic_structures.py
+++ basic_structures.py
@@ -8,23 +8,22 @@ import sys
 #print (sys.argv[1])
 #print (sys.argv[2])
 
-
 opt = sys.argv[1]
 person = sys.argv[2]
 
-if opt == '-h':
-    i = 0
-    while i < 12:
-        i = i + 1
-        print("Hello, " + person + "! (" +str(i) + ")")
-elif opt == '-g':
-    i = 0
-    while i < 12:
-        i = i + 1
-        print("Goodbye, " + person + "! (" +str(i) + ")")
         
- 
+# Greeting Function
+def greeting(greetingType):
+     i = 0
+     while i < 12:
+         i = i + 1
+         print(greetingType + ", " + person + "! (" +str(i) + ")")
 
 
-
+# Main Code
+if opt == '-h':
+     greeting('Hello')
+elif opt == '-g':
+     greeting('Goodbye')
+    
 
